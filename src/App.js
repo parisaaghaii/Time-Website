74e78af2849f91586e9819664edbb554b1c10d26
parisa aghaii <parisaa.aghaii@gmail.com>
2021-11-07 17:53:52 +0330

    changing Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import react from 'react';
+import React from 'react';
 import './App.css';
-import backgroundtop from './assets/images/cc.png';
+import './timedate.css';
+import './changindate.css';
+import backgroundtop from './assets/images/background-top.png';
+import Button from './component/Button';
 
 function App() {
   return (
-    <div className="App">
+    <div className="time-website">
       <img src={backgroundtop} className="backgroundImage" />
-      
+      <div className="time-website-boxs ">
+        <div className="time-website-time-top">
+          <div className="time-website-time-top-inner">jooihnubujb</div>
+        </div>
+        <div className="time-website-time">
+          <Button />
+        </div>
+        <div>
+          This establishes the main-axis, thus defining the direction flex items
+          are placed in the flex container. Flexbox is (aside from optional
+          wrapping) a single-direction layout concept. Think of flex items as
+          primarily laying out either in horizontal rows or vertical columns.
+        </div>
+
+        <div className="time-website-date">
+          edgbjlergjmeThis establishes the main-axis, thus defining the dir
+        </div>
+      </div>
+      <div className="time-website-boxs ">
+        edgbjlergjmeThis establishes the main-axis, thus defining the direction
+        flex items are placed in the flex container. Flexbox is (aside from
+        optional wrapping) a single-direction layout concept. Think of flex
+        items as primarily laying out either in horizontal rows or vertical
+        columns.r
+      </div>
+      <div className="time-website-boxs ">
+        edgbjlergjmeThis establishes the main-axis, thus defining the direction
+        flex items are placed in the flex container. Flexbox is (aside from
+        optional wrapping) a single-direction layout concept. Think of flex
+        items as primarily laying out either in horizontal rows or vertical
+        columns.r
+      </div>
+      <div className="time-website-boxs ">
+        edgbjlergjmeThis establishes the main-axis, thus defining the direction
+        flex items are placed in the flex container. Flexbox is (aside from
+        optional wrapping) a single-direction layout concept. Think of flex
+        items as primarily laying out either in horizontal rows or vertical
+        columns.r
+      </div>
     </div>
   );
 }

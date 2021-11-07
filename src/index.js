96919ae04e41fef148d291378e6ea07dc4657db8
parisa aghaii <parisaa.aghaii@gmail.com>
2021-11-07 17:55:33 +0330

    import font

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./fonts/B-Nazanin.css";
+import "./fonts/BTitr.css";
 
 ReactDOM.render(
   <React.StrictMode>

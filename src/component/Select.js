9b088572c1dce432764fb82e0c46800fd7d8529b
parisa aghaii <parisaa.aghaii@gmail.com>
2021-11-08 17:53:27 +0330

    create select

---
+++ src/component/Select.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Select.css";
+
+function Select({
+  list = [],
+  placeholder = "",
+  selected = "",
+  onChange = () => {},
+  children = undefined,
+  classNameSelecttext = "",
+}) {
+  return (
+    <form className="formDes ">
+      <select
+        className={`selDesign ${classNameSelecttext}`}
+        value={selected}
+        onChange={onChange}
+      >
+        {!selected && (
+          <option key={"namelist"} value={""}>
+            {placeholder}
+          </option>
+        )}
+        {list.map((item, index) => {
+          return (
+            <option key={index} value={item}>
+              {item}
+            </option>
+          );
+        })}
+        {children}
+      </select>
+    </form>
+  );
+}
+export default Select;
